2bde8b387d25fe8dc68b3f7fec32489c3d2422ee
xcheneric <xcheneric@gmail.com>
2015-07-09 00:32:21 -0700

    TransportationActivity now has 3 seperate methods for buttons.

---
+++ app/src/main/java/com/ironsquishy/biteclub/CarFragment.java
@@ -0,0 +1,17 @@
+package com.ironsquishy.biteclub;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Eric on 7/8/2015.
+ */
+public class CarFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.car_fragment, container, false);
+    }
+}
