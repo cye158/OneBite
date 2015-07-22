e17bf921045006d60d2158d0909b8d40245c571d
Clarence Yamane Enriquez <cye0105@gmail.com>
2015-07-21 22:08:27 -0700

    made inputLocation to fragment; fixed the filterOption; changed color of BG; added

--- app/src/main/java/com/ironsquishy/biteclub/InputLocationActivity.java
+++ app/src/main/java/com/ironsquishy/biteclub/InputLocationActivity.java
@@ -1,6 +1,7 @@
 package com.ironsquishy.biteclub;
 
-import android.app.DialogFragment;;
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,16 +12,18 @@ import android.widget.Toast;
 /**
  * Created by Renz on 7/2/2015.
  * @author Renz
- * Description: Prompts the user for an address location
+ * Description: Prompts the user for an address location. The input is then pass to the
+ *              location handler to get the lcoation. *Current is in the works*
  **/
 public class InputLocationActivity extends DialogFragment implements View.OnClickListener {
 
-    Button Enter;// LocSerSwitch;
-    String address;
+    Button Enter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_input_location, null);
         Enter = (Button) view.findViewById(R.id.enterButton);
+        Enter.setOnClickListener(this);
         setCancelable(false);
         return view;
     }
@@ -28,13 +31,9 @@ public class InputLocationActivity extends DialogFragment implements View.OnClic
     @Override
     public void onClick(View view){
         if (view.getId()==R.id.enterButton){
-            // TODO for address input by the user.
             dismiss();
-            Toast.makeText(getActivity(), "Your at: "+ address,
-                    Toast.LENGTH_SHORT).show();
-        }else{
-            dismiss();
-            Toast.makeText(getActivity(), "Please enter a starting location",
+            startActivity(new Intent(this.getActivity(), TransportationActivity.class));
+            Toast.makeText(getActivity(), "Location has been entered",
                     Toast.LENGTH_SHORT).show();
         }
     }
