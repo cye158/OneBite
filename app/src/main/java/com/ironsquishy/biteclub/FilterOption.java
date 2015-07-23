b76e49b22c44e8639ae9a6714e7d08a67fba3ed2
Clarence Yamane Enriquez <cye0105@gmail.com>
2015-07-23 14:58:20 -0700

    pulling

--- app/src/main/java/com/ironsquishy/biteclub/FilterOption.java
+++ app/src/main/java/com/ironsquishy/biteclub/FilterOption.java
@@ -3,8 +3,11 @@ package com.ironsquishy.biteclub;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,6 +21,9 @@ import java.util.ArrayList;
  **/
 public class FilterOption extends DialogFragment {
 
+    final int FILTER_LIST_SIZE = 14;
+    SharedPreferences preferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -26,6 +32,8 @@ public class FilterOption extends DialogFragment {
         final String[] foodCuisine = { "NewAmerican", "Mexican", "Chinese", "Filipino", "Italian",
                 "Japanese", "Korean", "Vietnamese", "Thai", "Vegetarian", "Creperies", "Cafe",
                 "Desserts", "Seafood" };
+
+        //load pref
 
         /*Alert dialog declaration*/
         AlertDialog.Builder filterDialog = new AlertDialog.Builder(getActivity());
@@ -37,6 +45,7 @@ public class FilterOption extends DialogFragment {
             public void onClick(DialogInterface dialog, int which, boolean isChecked) {
                 if (isChecked) {
                     arrayFilter.add(which);
+                    saveFilter(,isChecked);
                 } else if (arrayFilter.contains(which)) {
                     arrayFilter.remove(Integer.valueOf(which));
                 }
@@ -44,6 +53,7 @@ public class FilterOption extends DialogFragment {
         });
 
         filterDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            //commit pref
 
             /*The array is then converted to a string with ',' to separate each item */
             String addToFilter = "";
@@ -73,5 +83,17 @@ public class FilterOption extends DialogFragment {
         setCancelable(false);
         AlertDialog objDialog = filterDialog.create();
         return objDialog;
+    }
+
+    public void saveFilter(int index, Boolean isChecked) {
+        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("check" + index, isChecked);
+        editor.commit();
+    }
+
+    public void loadFilter(int index) {
+        SharedPreferences sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
+        sharedPreferences.getBoolean("check" + index, false);
     }
 }
