fd2acf313c6a58e1e965db4b7714ac7d283cfb43
eyao06 <eyao06@yahoo.com>
2015-07-26 01:50:16 -0700

    added yelp info in result activity

--- app/src/main/java/com/ironsquishy/biteclub/ResultActivity.java
+++ app/src/main/java/com/ironsquishy/biteclub/ResultActivity.java
@@ -3,38 +3,84 @@ package com.ironsquishy.biteclub;
 import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-
+import ApiManagers.DatabaseManager;
+import ApiManagers.RestaurantManager;
+import apihelpers.YelpApiHandler.Restaurant;
 
 
 /**
- * @author Edward Yao
- *         Description: Result activity, shows the restaurant result of the randomized search
- *         allows the user to change the distance of the search via modes of transportatiom
- *         can obtain additional information of the restaurant
+ * @author Allen Space
+ *         Description: Menu  activity with google maps fragment.
  */
-public class ResultActivity extends Activity {
+public class ResultActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener {
 
     /**
      * Data Fields
      */
+    private static TextView mResultText;
+    private static String mRandomStringName;
+    private static SwipeRefreshLayout swipeRefreshLayout;
+    private static RestaurantManager mRestaurantManager;
+    private static Restaurant mRestaurant;
+
+    private static TextView addToData;
+    private static ImageView mYelpImage;
+
+    private static DatabaseManager mDatabaseManager;
+
+    private static Context mContext;
+
+    private AlertDialog.Builder filterDialog;
+    private String inputFilter = "\n Filtered: ";
+
+    private static TextView mExtYelpInfo, mMoreYelpInfo;
+
     private static TextView expandInfo, collapseInfo;
     private static LinearLayout mLinearLayout;
     private static ValueAnimator mExpandAnimator;
     private static ImageView car_button, bus_button, walk_button;
 
+    /**
+     * @Author Allen Space
+     * Description: To create the menu activity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        mContext = this;
+
+        mResultText = (TextView) findViewById(R.id.resultText);
+
+        mYelpImage = (ImageView) findViewById(R.id.YelpImage);
+
+        addToData = (TextView) findViewById(R.id.checkToAddFav);
+
+        mDatabaseManager = new DatabaseManager(this);
+
+        mExtYelpInfo = (TextView) findViewById(R.id.YelpInfo);
+
+        mMoreYelpInfo = (TextView) findViewById(R.id.MoreYelpInfo);
+
+        mRestaurantManager = new RestaurantManager();
+
+        randomizeYelpResponse();
+
 
         expandInfo = (TextView) findViewById(R.id.showInfo);
         collapseInfo = (TextView) findViewById(R.id.hideInfo);
@@ -67,9 +113,9 @@ public class ResultActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                if (mLinearLayout.getVisibility()==View.GONE) {
+                if (mLinearLayout.getVisibility() == View.GONE) {
                     expand();
-                }else{
+                } else {
                     collapse();
                 }
             }
@@ -81,6 +127,101 @@ public class ResultActivity extends Activity {
                 collapse();
             }
         });
+    }
+
+    /**
+     * Check for favorite.*
+     */
+    public void checkFavAdd(View view) {
+        //Add to result in text view to data.
+        mDatabaseManager.addToDatabase(mRandomStringName);
+
+        //TODO There should be a check to see if it has already been added to favorites, then the toast message should say "already added"
+        Toast.makeText(getApplicationContext(), "Added to favorites.",
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    /**
+     * Called when the user clicks the navigation floating action button - Eric
+     */
+    public void toNavi(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Called when the user clicks the untappdFeed button - Eric
+     */
+    public void toInfo(View view) {
+        Intent intent = new Intent(this, UntappdActivity.class);
+        intent.putExtra("restname", mRestaurant.getmRestName());
+        startActivity(intent);
+    }
+
+    /** Called when the user clicks the Filter button - Eric */
+    /**
+     * Revised by Renz
+     */
+    public void toFilter(View view) {
+        FilterOption dialog = new FilterOption();
+        dialog.show(getFragmentManager(), "Filter Dialog Box");
+    }
+
+    /**
+     * @Author Allen Space
+     * Description: On start, creates process dialog
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    /**
+     * @author Allen Space
+     * Description: ReShuffles the yelp data.
+     * And displays on screen.
+     */
+    private void randomizeYelpResponse() {
+
+        //Get a random restuarant.
+        mRestaurant = mRestaurantManager.getRandRestCar();
+
+        //Set the Text name.
+        mResultText.setText(mRestaurant.getmRestName());
+
+        //Set the Descripiton and ratings
+
+        mExtYelpInfo.setText("Address: " + "\n" + "Ratings: " + String.valueOf(mRestaurant.getmRatings()));
+        mMoreYelpInfo.setText(mRestaurant.getmDescription());
+
+    }
+
+    /**
+     * @Author Eric Chen
+     * Description: To create pull down to refresh.
+     */
+    private void swipeRefresh() {
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        swipeRefreshLayout.setOnRefreshListener(this);
+    }
+
+    @Override
+    public void onRefresh() {
+        //Created to simulate loading.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do stuff here.
+                randomizeYelpResponse();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        }, 1000);
     }
 
     /**
