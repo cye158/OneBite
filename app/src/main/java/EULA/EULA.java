becee2ca07f3bf273d0a69fad59761d99b1d2ede
Guan <guanlin92@yahoo.com>
2015-07-29 22:11:50 -0700

    added EULA and alot of text

--- app/src/main/java/EULA/EULA.java
+++ app/src/main/java/EULA/EULA.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
 
 import com.ironsquishy.biteclub.R;
+import com.ironsquishy.biteclub.SplashActivity;
 
 public class EULA {
 
@@ -36,19 +38,23 @@ public class EULA {
         // the eulaKey changes every time you increment the version number in the AndroidManifest.xml
         final String eulaKey = EULA_PREFIX + versionInfo.versionCode;
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mActivity);
-        boolean hasBeenShown = prefs.getBoolean(eulaKey, false);
+        boolean hasBeenShown = false; //prefs.getBoolean(eulaKey, false);
         if(hasBeenShown == false){
 
             // Show the Eula
             String title = mActivity.getString(R.string.app_name) + " v" + versionInfo.versionName;
 
             //Includes the updates as well so users know what changed. 
-            String message = mActivity.getString(R.string.updates) + "\n\n" + mActivity.getString(R.string.eula);
+            String message = mActivity.getString(R.string.eula_title)
+                    + "\n\n" + mActivity.getString(R.string.eula_copyright)
+                    + "\n\n" + mActivity.getString(R.string.eula_license)
+                    + "\n\n" + mActivity.getString(R.string.eula_disclaimer);
 
             AlertDialog.Builder builder = new AlertDialog.Builder(mActivity)
                     .setTitle(title)
                     .setMessage(message)
-                    .setPositiveButton(android.R.string.ok, new Dialog.OnClickListener() {
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.agree_eula, new Dialog.OnClickListener() {
 
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
@@ -57,9 +63,11 @@ public class EULA {
                             editor.putBoolean(eulaKey, true);
                             editor.commit();
                             dialogInterface.dismiss();
+                            mActivity.startActivity(new Intent(mActivity, SplashActivity.class));
+                            mActivity.finish();
                         }
                     })
-                    .setNegativeButton(android.R.string.cancel, new Dialog.OnClickListener() {
+                    .setNegativeButton(R.string.disagree_eula, new Dialog.OnClickListener() {
 
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
@@ -69,7 +77,9 @@ public class EULA {
 
                     });
             builder.create().show();
+        } else {
+            mActivity.startActivity(new Intent(mActivity, SplashActivity.class));
+            mActivity.finish();
         }
     }
-
 }
