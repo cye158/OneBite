1c17bf7c2b7b6b2890e1eac0eafd22b4c1e922ab
ironsquishy <spacecore88@gmail.com>
2015-07-31 15:54:41 -0700

    Filter fix, RestMananagre should send back the filter Rslt.

---
+++ app/src/main/java/apihelpers/googleapis/LocationHelper.java
@@ -0,0 +1,73 @@
+package apihelpers.googleapis;
+
+import android.location.Location;
+
+/**
+ * Created by Allen Space on 7/31/2015.
+ */
+public class LocationHelper {
+
+    /**Data Fields*/
+    private Location mOrigin;
+    private Location mDest;
+
+    private double mOriginLatidude;
+    private double mOriginLongitude;
+
+
+    private double mDestLatitude;
+    private double mDestLongitude;
+
+    public LocationHelper()
+    {
+        //defualt constructor.
+    }
+
+    public LocationHelper(double pOriginLat, double pOriginLng, double pDestLat, double pDestLng)
+    {
+        this.mOriginLatidude = pOriginLat;
+        this.mOriginLongitude = pOriginLng;
+
+        this.mDestLatitude = pDestLat;
+        this.mDestLongitude = pDestLng;
+
+    }
+
+    public LocationHelper(Location pOrigin, Location pDest)
+    {
+        this.mOrigin = pOrigin;
+        this.mDest = pDest;
+    }
+
+    public double getDistanceFromOrigin()
+    {
+        //Set origin location.
+        Location origin = new Location("origin");
+        origin.setLatitude(mOriginLatidude);
+        origin.setLongitude(mOriginLongitude);
+
+        //Set given Restuarant location.
+        Location location = new Location("Dest");
+        location.setLatitude(mDestLatitude);
+        location.setLongitude(mDestLongitude);
+
+        return origin.distanceTo(location);
+    }
+
+    public double getDistanceFromOrigin(double pOriginLat, double pOriginLng, double pDestLat,
+                                        double pDestLng)
+    {
+        //Set origin location.
+        Location origin = new Location("origin");
+        origin.setLatitude(pOriginLat);
+        origin.setLongitude(pOriginLng);
+
+        //Set given Restuarant location.
+        Location location = new Location("Dest");
+        location.setLatitude(pDestLat);
+        location.setLongitude(pDestLng);
+
+        return origin.distanceTo(location);
+    }
+
+}
