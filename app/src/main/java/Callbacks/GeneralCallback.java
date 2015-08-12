4948ce946cecf78a360d0f9c5a17f415e47aa0a6
ironsquishy <spacecore88@gmail.com>
2015-08-12 10:42:01 -0700

    Pulling before presentation.

--- app/src/main/java/Callbacks/GeneralCallback.java
+++ app/src/main/java/Callbacks/GeneralCallback.java
@@ -4,13 +4,9 @@ package Callbacks;
  * Created by Allen Space on 7/22/2015.
  */
 abstract public class GeneralCallback implements Runnable{
-
     @Override
     public void run() {
 
     }
-
     abstract public void runWithResponse(Object object);
-
-
 }
